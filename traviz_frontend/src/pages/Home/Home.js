cbb7c035bf4efc6bb566b67e3dd2038a7c3fffec
Vaastav Anand <vaastav.anand05@gmail.com>
2019-11-13 11:41:49 -0800

    Add basic divs

--- traviz_frontend/src/pages/Home/Home.js
+++ traviz_frontend/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import TraceService from "../../services/TraceService/TraceService";
+import "./Home.css";
 
 class Home extends React.Component {
     constructor() {
@@ -16,8 +17,12 @@ class Home extends React.Component {
         this.traceService.getAllTraces().then(response => {
             this.setState({ traces: response});
         });
-    }
+        const d3_script = document.createElement("script");
+        d3_script.async = false;
+        d3_script.src = "https://square.github.io/crossfilter/d3.v3.min.js";
+        document.body.appendChild(d3_script);
 
+<<<<<<< 7a1d319ef40a9537eaaa05121ef364dcbd82c315
     renderTraces = () => {
         return this.state.traces.map((trace, id) => {
             return (
@@ -26,12 +31,32 @@ class Home extends React.Component {
                 </li>
             )
         })
+=======
+        const cf_script = document.createElement("script");
+        cf_script.asynf = false;
+        cf_script.src = "https://square.github.io/crossfilter/crossfilter.v1.min.js";
+        document.body.appendChild(cf_script);
+>>>>>>> Add basic divs
     }
 
     render() {
         return (
             <div>
-                <ul>{this.renderTraces()}</ul>
+                <div id="charts">
+                    <div id="events-chart" class="chart">
+                        <div class="title">Number of Events</div>
+                    </div>
+                    <div id="duration-chart" class="chart">
+                        <div class="title">Duration</div>
+                    </div>
+                    <div id="date-chart" class="chart">
+                        <div class="title">Date</div>
+                    </div>
+                </div>
+
+                <div id="lists">
+                    <div id="trace-list" class="list"></div>
+                </div>
             </div>
         );
     }
