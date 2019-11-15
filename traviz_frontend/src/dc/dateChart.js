ca50e6423ae444a54f87476e14f24d387b06d32d
Vaastav Anand <vaastav.anand05@gmail.com>
2019-11-14 17:01:03 -0800

    Add basic datechart

---
+++ traviz_frontend/src/dc/dateChart.js
@@ -0,0 +1,24 @@
+import React from "react";
+import * as dc from "dc";
+import * as d3 from "d3";
+import { ChartTemplate } from "./chartTemplate";
+
+const dateChartFunc = (divRef, ndx) => {
+    const dateChart = dc.barChart(divRef);
+    const dimension = ndx.dimension(function(d) { return d.dd;});
+    const dates = dimension.group(d3.timeDay);
+    dateChart
+    .dimension(dimension)
+    .group(dates)
+    .round(d3.timeDay.round)
+    .x(d3.scaleTime()
+        .domain([d3.min(dimension.bottom(1)[0].dd), d3.max(dimension.top(1)[0].dd)])
+        .rangeRound([0, 10 * 90])
+    );
+
+    return dateChart;
+}
+
+export const DateChart = props => (
+    <ChartTemplate chartFunction={dateChartFunc} title="Days of Traces" />
+)
