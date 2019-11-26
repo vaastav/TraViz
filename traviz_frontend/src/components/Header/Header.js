e3635707cc1b056b7d111c371123b693c8da29f3
Vaastav Anand <vaastav.anand05@gmail.com>
2019-11-25 17:16:39 -0800

    Add navbar

--- traviz_frontend/src/components/Header/Header.js
+++ traviz_frontend/src/components/Header/Header.js
@@ -4,7 +4,11 @@ import './Header.css';
 function Header() {
     return (
         <header>
-            <h1> TraViz </h1>
+            <ul>
+                <li><a href="/" class="navbar"><h1>TraViz </h1></a></li>
+                <li><a href="/source" class="navbar"><h2>Source </h2></a></li>
+                <li><a href="/dependency" class="navbar"><h2>Dependency </h2></a></li>
+            </ul>
         </header>
     );
 }
