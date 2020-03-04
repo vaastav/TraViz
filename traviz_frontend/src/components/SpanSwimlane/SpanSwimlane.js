136872bf4c286a690840a2e994fec0ee996ba53d
Matt <matheusstolet@hotmail.com>
2020-03-04 01:14:01 -0800

    Highlight selected bars

--- traviz_frontend/src/components/SpanSwimlane/SpanSwimlane.js
+++ traviz_frontend/src/components/SpanSwimlane/SpanSwimlane.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react"
 import * as d3v3 from "d3-v3"
-import * as dc from "dc"
-import * as crossfilter from "crossfilter2/crossfilter";
 import './SpanSwimlane.css'
 import TraceService from "../../services/TraceService/TraceService";
 import TaskService from "../../services/TaskService/TaskService";
@@ -115,44 +113,33 @@ function setChildSpans(spanMap, eventMap) {
     });
 }
 
-// Mark the spans that are in the critical path.
-function markCriticalPath(spanMap, currentSpanId, parentSpanClass) {
-    if (currentSpanId === null || currentSpanId === undefined) {
-        return;
+function threadIdToIdMap(spans) {
+    let map = new Map();
+    for (let i = 0; i < spans.length; i++) {
+        let span = spans[i];
+        map.set(span.id, i);
     }
-    let currentSpan = spanMap.get(currentSpanId);
-
-    if (currentSpan.childSpanId !== null && currentSpan.childSpanId !== undefined) {
-        for (let i = 0; i < currentSpan.childSpanId.length; i++) {
-            let childSpanId = currentSpan.childSpanId[i];
-            let childSpan = spanMap.get(childSpanId);
-            let inCriticalPath = true;
-            for (let j = 0; j < currentSpan.childSpanId.length; j++) {
-                let comparisonSpanId = currentSpan.childSpanId[j];
-                let comparisonSpan = spanMap.get(comparisonSpanId);
-                if (childSpan.start > comparisonSpan.start && childSpan.end < comparisonSpan.end) {
-                    inCriticalPath = false;
-                    break
-                }
-            }
-            if (inCriticalPath && parentSpanClass === "criticalPath") {
-                childSpan.class = "criticalPath";
-            }
-            markCriticalPath(spanMap, childSpanId, childSpan.class);
-        }
-    }
+    return map;
 }
 
-// function threadIdToIdMap(spans) {
-//     let map = new Map()
-//     for (let i = 0; i < spans.length; i++) {
-//         let span = spans[i]
-//         map.set(i, span)
-//     }
-//     spans.forEach(s => {
-//         map.set()
-//     })
-// }
+function idToThreadIdMap(spans) {
+    let map = new Map();
+    for (let i = 0; i < spans.length; i++) {
+        let span = spans[i];
+        map.set(i, span.id);
+    }
+    return map;
+}
+
+function setSelectedSpan(spans, threadId) {
+    spans.forEach(span => {
+        if (span.id === threadId) {
+            span.class = "selected"
+        } else {
+            span.class = "normal"
+        }
+    })
+}
 
 class SpanSwimlane extends Component {
     constructor(props) {
@@ -187,16 +174,11 @@ class SpanSwimlane extends Component {
         let spans = createSpans(this.state.trace);
         let spanMap = makeSpanMap(spans);
         let eventMap = makeEventMap(events);
-
-        // Mark first span as in critical path
-        if (spans.length > 0) {
-            spans[0].class = "criticalPath"
-        }
-
+        let idToThreadId = idToThreadIdMap(spans);
+        let threadIdToId = threadIdToIdMap(spans);
         // Find the child span of each span and then use that to
         // find the critical path of a trace.
         setChildSpans(spanMap, eventMap);
-        markCriticalPath(spanMap, spans[0].id, "criticalPath");
 
         // Get start, end and duration
         let start = getStartTime(this.state.trace);
@@ -212,6 +194,9 @@ class SpanSwimlane extends Component {
 
         var ext = d3v3.extent(lanes, function (d) { return d.id; });
         var y2 = d3v3.scale.linear().domain([ext[0], ext[1] + 1]).range([0, miniHeight]);
+
+        let paths = getPaths(spans)
+
 
         var chart = d3v3.select('#swimlane')
             .append('svg')
@@ -236,7 +221,7 @@ class SpanSwimlane extends Component {
             .attr('stroke', "#979797");
 
         // Draw the last lane so we have a box
-        let lastLane = lanes[lanes.length - 1] 
+        let lastLane = lanes[lanes.length - 1]
         mini.append('g').selectAll('.laneLines')
             .data(lanes)
             .enter().append('line')
@@ -259,99 +244,85 @@ class SpanSwimlane extends Component {
             .attr("class", "tooltip")
             .style("opacity", 0);
 
-        this.createHistograms(tasks)
-        display();
+        createHistograms();
 
-        function display() {
+        function createHistograms() {
+            // Define size of one histogram
+            let histMargin = { top: 20, right: 20, bottom: 15, left: 5 }
+            let histWidth = 100
+            let histHeight = 100
 
-            var rects, labels
-            // , minExtent = brush.extent()[0]
-            // , maxExtent = brush.extent()[1]
-            // , visItems = spans.filter(function (d) { return d.start < maxExtent && d.end > minExtent })
-            // , visEvents = events.filter(function (d) { return d.HRT <= maxExtent && d.HRT >= minExtent })
-            // , visLines = connectingLines.filter(function (d) {
-            //     return (d.origin.HRT <= maxExtent && d.origin.HRT >= minExtent) ||
-            //         (d.destination.HRT <= maxExtent && d.destination.HRT >= minExtent)
-            // });
+            tasks.forEach(task => {
+                let logData = task.Data.filter(d => d != 0).map(d => Math.log(d))
+                logData = task.Data
 
-            // mini.select('.brush').call(brush.extent([minExtent, maxExtent]));
+                let minDur = Math.min.apply(Math, logData)
+                let maxDur = Math.max.apply(Math, logData)
 
-            // x1.domain([minExtent, maxExtent]);
+                let histSvg = d3v3.select('#hists')
+                    .append("svg")
+                    .on("mouseover", function () {
+                        setSelectedSpan(spans, task.ThreadID)
+                        let miniItems = mini.selectAll('miniItems').data(getPaths(spans))
+                        miniItems.enter().append('path')
+                            .attr('class', function (d) {
+                                return "miniItem " + d.class 
+                            })
+                            .attr('d', function (d) { return d.path; });
+                        miniItems.exit().remove()
+                    })
+                    .on("mouseout", function () {
+                        setSelectedSpan(spans, task.ThreadID)
+                        let miniItems = mini.selectAll('miniItems').data(getPaths(spans))
+                        miniItems.enter().append('path')
+                            .attr('class', function (d) { return "miniItem " + d.class  })
+                            .attr('d', function (d) { return d.path; });
+                        miniItems.exit().remove()
+                    })
+                    .attr("width", histWidth + histMargin.left + histMargin.right)
+                    .attr("height", histHeight + histMargin.top + histMargin.bottom)
+                    .append("g")
+                    .attr('class', "histsvg" + threadIdToId.get(task.ThreadID))
+                    .attr("transform",
+                        "translate(" + histMargin.left + "," + histMargin.top + ")")
 
-            // update the axis
-            // main.select('.main.axis').call(x1Axis);
+                let histX = d3v3.scale.linear()
+                    .domain([minDur, maxDur])
+                    .range([0, histWidth]);
 
-            // upate the item rects
-            // rects = itemRects.selectAll('rect')
-            //     .data(visItems, function (d) { return d.id; })
-            //     .attr('x', function (d) { return x1(d.start); })
-            //     .attr('width', function (d) { return x1(d.end) - x1(d.start); })
-            //     .attr('stroke', 'red')
+                let data = d3v3.layout.histogram()
+                    .bins(histX.ticks(15))(logData);
 
-            // rects.enter().append('rect')
-            //     .attr('x', function (d) { return x1(d.start); })
-            //     .attr('y', function (d) { return y1(d.y_id) + .1 * y1(1) + 0.5; })
-            //     .attr('width', function (d) { return x1(d.end) - x1(d.start); })
-            //     .attr('height', function (d) { return .8 * y1(1); })
-            //     .attr('class', function (d) { return 'mainItem ' + d.class; })
-            //     .attr('stroke', 'red')
+                let histYMax = d3v3.max(data, function (d) { return d.length });
+                let histYMin = d3v3.min(data, function (d) { return d.length });
 
-            // rects.exit().remove();
+                let histY = d3v3.scale.linear()
+                    .domain([0, histYMax])
+                    .range([histHeight, 0]);
 
-            // draw the event circles
-            // let circs = circles.selectAll(".circ")
-            //     .data(visEvents)
-            //     .attr("r", 0.1 * y1(1))
-            //     .attr("fill", "white")
-            //     .attr("stroke", "black")
-            //     .attr("cx", function (d) { return x1(d.HRT) })
-            //     .attr("cy", function (d) { return y1(threadToLaneMap.get(d.ThreadID)) + .5 * y1(1) + 0.5; })
-            //     .attr("class", "circ")
+                let bar = histSvg.selectAll(".bar")
+                    .data(data)
+                    .enter().append("g")
+                    .attr("class", "bar")
+                    .attr("transform", function (d) { return "translate(" + histX(d.x) + "," + histY(d.y) + ")"; });
 
-            // circs.enter().append("circle")
-            //     .attr("r", 0.1 * y1(1))
-            //     .attr("fill", "white")
-            //     .attr("stroke", "black")
-            //     .attr("cx", function (d) { return x1(d.HRT) })
-            //     .attr("cy", function (d) { return y1(threadToLaneMap.get(d.ThreadID)) + .5 * y1(1) + 0.5; })
-            //     .attr("class", "circ")
-            //     .on("mouseover", function (d) {
-            //         tooltipdiv.transition()
-            //             .duration(200)
-            //             .style("opacity", .9);
-            //         tooltipdiv.html("Event id: " + d.EventID + "<br/>" + "Timestamp: " + d.Timestamp + "<br/>" + "Label: " + d.Label)
-            //             .style("left", (d3v3.event.pageX) + "px")
-            //             .style("top", (d3v3.event.pageY - 28) + "px")
-            //     })
-            //     .on("mouseout", function (d) {
-            //         tooltipdiv.transition()
-            //             .duration(500)
-            //             .style("opacity", 0);
-            //     })
+                bar.append("rect")
+                    .attr("x", 1)
+                    .attr("width", (histX(data[0].dx) - histX(0)) - 1)
+                    .attr("height", function (d) { return histHeight - histY(d.y); })
+                    .attr("fill", function (d) {
+                        let min = Math.min.apply(Math, d);
+                        let max = Math.max.apply(Math, d);
+                        let logDur = Math.log(task.Duration);
+                        logDur = task.Duration
+                        if (logDur !== null && logDur >= min && logDur <= max) {
+                            return "#fff600" // Yellow
+                        } else {
+                            return "#8EB200" // Green
+                        }
+                    });
+            })
 
-            // circs.exit().remove();
-
-            // draw the lines between parent and child events
-            // let lins = lines.selectAll(".lin")
-            //     .data(visLines)
-            //     .attr("width", 200)
-            //     .attr("x1", function (d) { return x1(d.origin.HRT) })
-            //     .attr("y1", function (d) { return y1(threadToLaneMap.get(d.origin.ThreadID)) + .5 * y1(1) + 0.5; })
-            //     .attr("x2", function (d) { return x1(d.destination.HRT) })
-            //     .attr("y2", function (d) { return y1(threadToLaneMap.get(d.destination.ThreadID)) + .5 * y1(1) + 0.5; })
-            //     .attr("class", "lin");
-
-            // lins.enter().append("line")
-            //     .attr("width", 200)
-            //     .attr("x1", function (d) { return x1(d.origin.HRT) })
-            //     .attr("y1", function (d) { return y1(threadToLaneMap.get(d.origin.ThreadID)) + .5 * y1(1) + 0.5; })
-            //     .attr("x2", function (d) { return x1(d.destination.HRT) })
-            //     .attr("y2", function (d) { return y1(threadToLaneMap.get(d.destination.ThreadID)) + .5 * y1(1) + 0.5; })
-            //     .attr("class", "lin");
-
-            // lins.exit().remove();
-
-            d3v3.select("#cont").attr("align", "center")
         }
 
         function getPaths(items) {
@@ -370,76 +341,6 @@ class SpanSwimlane extends Component {
             }
             return result;
         }
-    }
-
-    createHistograms(tasks) {
-        // Define size of one histogram
-        let histMargin = { top: 20, right: 20, bottom: 15, left: 5 }
-        let histWidth = 100
-        let histHeight = 100
-
-        tasks.forEach(task => {
-            let logData = task.Data.filter(d => d != 0).map(d => Math.log(d))
-            logData = task.Data
-
-            let minDur = Math.min.apply(Math, logData)
-            let maxDur = Math.max.apply(Math, logData)
-
-            console.log(task)
-            let histSvg = d3v3.select('#hists')
-                .append("svg")
-                .attr("width", histWidth + histMargin.left + histMargin.right)
-                .attr("height", histHeight + histMargin.top + histMargin.bottom)
-                .append("g")
-                .attr('class', "s")
-                .attr("transform",
-                    "translate(" + histMargin.left + "," + histMargin.top + ")")
-                .on("mouseover", function() {
-                    console.log("mouseover")
-                    d3v3.select("s")
-                    .attr("fill", "red");
-                })
-                .on("mouseout", function() {
-                    console.log("mouseout")
-                })
-
-            let histX = d3v3.scale.linear()
-                .domain([minDur, maxDur])
-                .range([0, histWidth]);
-
-            let data = d3v3.layout.histogram()
-                .bins(histX.ticks(15))(logData);
-
-            let histYMax = d3v3.max(data, function (d) { return d.length });
-            let histYMin = d3v3.min(data, function (d) { return d.length });
-
-            let histY = d3v3.scale.linear()
-                .domain([0, histYMax])
-                .range([histHeight, 0]);
-
-            let bar = histSvg.selectAll(".bar")
-                .data(data)
-                .enter().append("g")
-                .attr("class", "bar")
-                .attr("transform", function (d) { return "translate(" + histX(d.x) + "," + histY(d.y) + ")"; });
-
-            bar.append("rect")
-                .attr("x", 1)
-                .attr("width", (histX(data[0].dx) - histX(0)) - 1)
-                .attr("height", function (d) { return histHeight - histY(d.y); })
-                .attr("fill", function (d) {
-                    let min = Math.min.apply(Math, d);
-                    let max = Math.max.apply(Math, d);
-                    let logDur = Math.log(task.Duration);
-                    logDur = task.Duration
-                    if (logDur !== null && logDur >= min && logDur <= max) {
-                        return "#fff600" // Red
-                    } else {
-                        return "#8EB200" // Green
-                    }
-                });
-        })
-
     }
 
     render() {
