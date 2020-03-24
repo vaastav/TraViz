187b7246ec49c1604cb83c08a97ed6b9df696eb2
Vaastav Anand <vaastav.anand05@gmail.com>
2020-03-24 15:23:45 -0700

    Add the magic tables that improve task aggregation data latency

---
+++ traviz_backend/views.sql
@@ -0,0 +1,5 @@
+USE traviz;
+
+CREATE TABLE trace_times AS select MIN(span_times.startTime) AS minStart, MAX(span_times.endTime) AS maxEnd, tasks.trace_id FROM span_timean_times, tasks WHERE span_times.span_id=tasks.span_id GROUP BY tasks.trace_id;
+
+CREATE TABLE molehillData AS SELECT span_times.startTime AS start, span_times.endTime AS end, tasks.ProcessName AS processName, tasks.Prosks.ProcessID AS processID, tasks.Operation AS operation FROM span_times, tasks WHERE span_times.span_id=tasks.span_id;
